2cd431aea792a014d28102218a1b7c9904df821f
AP1709 <pathakarpit9454@gmail.com>
2021-09-23 23:51:26 +0530

    nothing new

--- black jack final program after getting to know my bugs.py
+++ black jack final program after getting to know my bugs.py
@@ -48,7 +48,7 @@ def compare(user_score, computer_score):
 def play_game():
 
 
-  #Hint 5: Deal the user and computer 2 cards each using deal_card()
+ #Deal the user and computer 2 cards each using deal_card()
   user_cards = []
   computer_cards = []
   is_game_over = False
@@ -58,7 +58,7 @@ def play_game():
     computer_cards.append(deal_card())
 
   while not is_game_over:
-    #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+   # Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
     user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
     print(f"   Your cards: {user_cards}, current score: {user_score}")
@@ -67,7 +67,7 @@ def play_game():
     if user_score == 0 or computer_score == 0 or user_score > 21:
       is_game_over = True
     else:
-      #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+      #If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
       user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
       if user_should_deal == "y":
         user_cards.append(deal_card())
